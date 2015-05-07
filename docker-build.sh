4ebb7932cd432bcdefa01333ff326d2f206ead30
Michael Martin <mwm.cal@gmail.com>
2015-05-07 13:59:14 -0700

    removing crap from shell script

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
- 
-if [[ $1 != "" ]]
-then
-        wget -c "$1";
-        sudo shutdown -k 0;
-else
-        echo "Usage: sudo ./downloadAndShutdown <fileToDownload>";
-fi
 
 git pull origin
 
